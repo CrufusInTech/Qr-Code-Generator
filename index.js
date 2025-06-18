c9ac5156a1421e7b46131d8b489d0d538c593c19
Crufus <Chigozirimrufus@gmail.com>
2025-06-18 16:10:53 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,20 +4,29 @@
 3. Create a txt file to save the user input using the native fs node module.
 */
 
+// imported modules
+
 import { input } from '@inquirer/prompts';
 import qr from 'qr-image';
 import fs from 'fs';
 
 // The inquirer that is used to get user input.
+
 const answer = await input({ message: 'Enter your link: ' });
 console.log(`Your link is: ${answer}`);
 
-// The QR CODE IMAGE that is generated from the user entered URL.
+// The QR CODE IMAGE is generated from the user entered URL.
+const qrPng = qr.image(answer, { parse_url: true  });
 
-const qrPng = qr.image(answer, { parse_url: answer  });
-qrPng.pipe(fs.createWriteStream(answer + ".png"));
+// 3. Create a safe filename by stripping/escaping invalid characters
+const qrFilename = answer.replace(/[^a-z0-9]/gi, '_').toLowerCase() + ".png";
 
-const pngString = qr.imageSync(answer, { parse_url: answer });
+// 4. Save the QR image to a file
+qrPng.pipe(fs.createWriteStream(qrFilename));
 
+// 5. Optional: get the PNG as a string if needed elsewhere
+const pngString = qr.imageSync(answer, { type: 'png', parse_url: true });
+
+console.log(`QR code saved as: ${qrFilename}`);
 
 
